b961cd24758f25e744d7c5bddf4e8cb48dbed0ba
kosunhyo <asvs927@gmail.com>
2025-05-18 01:06:38 +0900

    1차

---
+++ Assets/Scripts/QuestData.cs
@@ -0,0 +1,14 @@
+
+
+public class QuestData 
+{
+    public string questName;
+    public int[] npcId;
+
+    public QuestData(string name, int[] npc)
+    {
+        questName = name;
+        npcId = npc;
+    }
+
+}
